a2806b9068a9cd920c59a5c1e0c522cb07525b04
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-03-09 00:28:30 -0300

    feature: add endpoint that deposit

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
 import express, { NextFunction, Request, Response } from "express";
 import { v4 as uuid } from "uuid"
+import { Customers } from "./interfaces/Customers";
 
 const app = express();
 app.use(express.json());
-
-interface Customers {
-  id: string
-  name: string
-  cpf: string
-  statement: []
-}
 
 const customers: Customers[] = [];
 
@@ -25,7 +19,7 @@ const verifyIfExistsAccount = (
   if (!customer)
     return response.status(404).json({ error: "Customer not found!" });
 
-  request.customer = customer
+  request.customer = customer;
   return next();
 }
 
@@ -49,7 +43,23 @@ app.post("/count", (request, response) => {
 app.get("/statement", verifyIfExistsAccount, (request, response) => {
   const { customer } = request;
 
-  return response.json(customer?.statement)
+  return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccount, (request, response) => {
+  const { customer } = request;
+  const { description, amount } = request.body;
+
+  const statementOperation = {
+    transactionId: uuid(),
+    description,
+    amount,
+    createdAt: new Date(),
+    type: 'credit'
+  };
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(3333, () => console.log("Server start on PORT 3333"));
